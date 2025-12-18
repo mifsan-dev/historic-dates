a5e41c2d1167fce1fa2483d100d687b0fd6a4f6e
Rishat Mukhametgaliev <mrishat.dev@gmail.com>
2025-12-18 14:05:22 +0300

    chore: fix webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -2,106 +2,107 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const isProd = process.env.NODE_ENV === "production";
-
-module.exports = {
-  entry: "./src/app/ui/index.tsx",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: isProd ? "[name].[contenthash].js" : "bundle.js",
-    clean: true,
-    publicPath: "/"
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"],
-    alias: {
-      "@": path.resolve(__dirname, "src")
-    }
-  },
-  devtool: isProd ? "source-map" : "eval-source-map",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/
-      },
-
-      {
-        test: /\.svg$/,
-        use: ["@svgr/webpack"]
-      },
-
-      // Sass Modules
-      {
-        test: /\.module\.s[ac]ss$/,
-        use: [
-          isProd ? MiniCssExtractPlugin.loader : "style-loader",
-          {
-            loader: "css-loader",
-            options: {
-              modules: {
-                localIdentName: isProd
-                  ? "[hash:base64:6]"
-                  : "[name]__[local]__[hash:base64:5]"
-              }
-            }
-          },
-          "postcss-loader",
-          {
-            loader: "sass-loader",
-            options: {
-              implementation: require("sass"),
-              sassOptions: {
-                quietDeps: true
-              }
-            }
-          }
-        ]
-      },
-
-      {
-        test: /\.css$/,
-        use: [
-          isProd ? MiniCssExtractPlugin.loader : "style-loader",
-          "css-loader"
-        ]
-      },
-
-      // Global Sass
-      {
-        test: /\.s[ac]ss$/,
-        exclude: /\.module\.s[ac]ss$/,
-        use: [
-          isProd ? MiniCssExtractPlugin.loader : "style-loader",
-          "css-loader",
-          "postcss-loader",
-          {
-            loader: "sass-loader",
-            options: {
-              implementation: require("sass"),
-              sassOptions: {
-                quietDeps: true
-              }
-            }
-          }
-        ]
+module.exports = (env, argv) => {
+  const isProd = argv.mode === "production";
+  return {
+    entry: "./src/app/ui/index.tsx",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: isProd ? "[name].[contenthash].js" : "bundle.js",
+      clean: true,
+      publicPath: "auto"
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"],
+      alias: {
+        "@": path.resolve(__dirname, "src")
       }
-    ]
-  },
-  devServer: {
-    static: "./public",
-    historyApiFallback: true,
-    port: 3000,
-    open: true
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html"
-    }),
-    isProd &&
-      new MiniCssExtractPlugin({
-        filename: "[name].[contenthash].css"
-      })
-  ].filter(Boolean)
+    },
+    devtool: isProd ? "source-map" : "eval-source-map",
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/
+        },
+
+        {
+          test: /\.svg$/,
+          use: ["@svgr/webpack"]
+        },
+
+        // Sass Modules
+        {
+          test: /\.module\.s[ac]ss$/,
+          use: [
+            isProd ? MiniCssExtractPlugin.loader : "style-loader",
+            {
+              loader: "css-loader",
+              options: {
+                modules: {
+                  localIdentName: isProd
+                    ? "[hash:base64:6]"
+                    : "[name]__[local]__[hash:base64:5]"
+                }
+              }
+            },
+            "postcss-loader",
+            {
+              loader: "sass-loader",
+              options: {
+                implementation: require("sass"),
+                sassOptions: {
+                  quietDeps: true
+                }
+              }
+            }
+          ]
+        },
+
+        {
+          test: /\.css$/,
+          use: [
+            isProd ? MiniCssExtractPlugin.loader : "style-loader",
+            "css-loader"
+          ]
+        },
+
+        // Global Sass
+        {
+          test: /\.s[ac]ss$/,
+          exclude: /\.module\.s[ac]ss$/,
+          use: [
+            isProd ? MiniCssExtractPlugin.loader : "style-loader",
+            "css-loader",
+            "postcss-loader",
+            {
+              loader: "sass-loader",
+              options: {
+                implementation: require("sass"),
+                sassOptions: {
+                  quietDeps: true
+                }
+              }
+            }
+          ]
+        }
+      ]
+    },
+    devServer: {
+      static: "./public",
+      historyApiFallback: true,
+      port: 3000,
+      open: true
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./public/index.html"
+      }),
+      isProd &&
+        new MiniCssExtractPlugin({
+          filename: "[name].[contenthash].css"
+        })
+    ].filter(Boolean)
+  };
 };
